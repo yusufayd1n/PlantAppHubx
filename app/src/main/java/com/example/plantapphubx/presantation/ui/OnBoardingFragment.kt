7ea1c1f95d823b17d38451487dc16caf71dea53a
yusufayd1n <yusufaydn24@gmail.com>
2025-04-17 22:07:08 +0300

    fragments and navGraph created

---
+++ app/src/main/java/com/example/plantapphubx/presantation/ui/OnBoardingFragment.kt
@@ -0,0 +1,31 @@
+package com.example.plantapphubx.presantation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.plantapphubx.databinding.FragmentOnBoardingBinding
+
+class OnBoardingFragment : Fragment() {
+
+    private var _binding: FragmentOnBoardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnBoardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
